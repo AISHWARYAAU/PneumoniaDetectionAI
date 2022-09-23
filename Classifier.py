36ecbb9397e3bf45d1f87fd8653fc73b15ffb4e5
Ahmedh Shamsudeen <82063965+codingperson98765@users.noreply.github.com>
2022-09-22 23:27:38 -0600

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -25,7 +25,7 @@ by Ahmedh Shamsudeen and Araiz Asad :sunglasses:
 
 filler_image = Image.open('AIface.jpeg')
 st.image(filler_image)
-st.write("Pneumonia is an infection that inflames the air sacs in one or both lungs. The air sacs may fill with fluid or pus, causing cough with phlegm or pus, fever, chills, and difficulty breathing.\n\n\nSource: https://www.mayoclinic.org/diseases-conditions/pneumonia/symptoms-causes/syc-20354204#:~:text=Pneumonia%20is%20an%20infection%20that,and%20fungi%2C%20can%20cause%20pneumonia.") 
+st.write("Pneumonia is an infection that inflames the air sacs in one or both lungs. The air sacs may fill with fluid or pus, causing cough with phlegm or pus, fever, chills, and difficulty breathing.\n\n\nSource: https://www.mayoclinic.org/diseases-conditions/pneumonia") 
 
 temp = st.file_uploader("Upload A Chest X-Ray Image")
 st.write("Link to chest X-ray Dataset for testing purposes if needed https://www.kaggle.com/datasets/paultimothymooney/chest-xray-pneumonia")
